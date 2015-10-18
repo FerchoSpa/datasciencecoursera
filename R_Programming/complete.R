725e8c4eb74e71aaca8ab5bcb90d8640c9ab9110
Fernando SPADARO <rfspadaro@gmail.com>
2015-10-18 08:41:08 -0700

    Finished complete.R

--- R_Programming/complete.R
+++ R_Programming/complete.R
@@ -12,4 +12,22 @@ complete <- function(directory, id = 1:332) {
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
+  #good<-complete.cases(d)
+  #sum(as.integer(good))
+  #ns<-append(ns, 4)
+  #obss<-append(obss, 4)
+  #df<-data.frame(foo=ns, bar=obss)
+  
+  ids <- integer()
+  nobs <- integer()
+  for (i in id) {
+    filename = paste0(directory, "/", sprintf("%03d", i), ".csv")
+    d <- read.csv(filename)
+    good<-complete.cases(d)
+    nCompleteCases <- sum(as.integer(good))
+    ids <- append(ids, i)
+    nobs <- append(nobs, nCompleteCases)
+  }
+  data.frame(id=ids, nobs=nobs)
+  
 }
